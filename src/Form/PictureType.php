1451955cbe2b1a5c2fc76869f4a0de4f223a565b
Bruno Dahlem <bruno.dahlem2@gmail.com>
2022-06-08 09:36:15 +0200

    Added pictures form

--- src/Form/PictureType.php
+++ src/Form/PictureType.php
@@ -2,34 +2,34 @@
 
 namespace App\Form;
 
-use App\Entity\Fishing;
+use App\Entity\Picture;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class FishingType extends AbstractType
+class PictureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('fishRace', TextType::class)
+            ->add('title', TextType::class)
+            ->add('place', TextType::class)
             ->add('date', DateType::class, [
                 'widget' => 'choice',
                 'format' => 'd-M-y',
             ])
-            ->add('place', TextType::class)
-            ->add('lure', TextType::class)
-            ->add('weather', TextType::class)
-            ->add('bait', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('photo')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Fishing::class,
+            'data_class' => Picture::class,
         ]);
     }
 }
