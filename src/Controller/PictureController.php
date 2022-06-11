4b0d9b96a52b0d48e85ae1520775ebd3d161c244
Bruno Dahlem <bruno.dahlem2@gmail.com>
2022-06-11 14:38:53 +0200

    Deleted photo on server OK

--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 #[Route('/picture')]
 class PictureController extends AbstractController
@@ -119,8 +120,13 @@ class PictureController extends AbstractController
     #[Route('/{id}', name: 'app_picture_delete', methods: ['POST'])]
     public function delete(Request $request, Picture $picture, PictureRepository $pictureRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$picture->getId(), $request->request->get('_token'))) {;
+        if ($this->isCsrfTokenValid('delete'.$picture->getId(), $request->request->get('_token'))) {
+            $filename = $picture->getPhoto();
             $pictureRepository->remove($picture, true);
+
+            // Supprimer la photo du dossier upload du serveur
+            $fs = new Filesystem();
+            $fs->remove($this->getParameter('photos_directory').'/'.$filename);
         }
 
         return $this->redirectToRoute('app_picture_index', [], Response::HTTP_SEE_OTHER);
