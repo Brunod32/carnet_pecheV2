26d904d9038f0aa62d02fb8f065b6ec81c2cba47
Bruno Dahlem <bruno.dahlem2@gmail.com>
2022-12-09 14:54:02 +0100

    Added hide/Show function in login

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 function openPictureInPopup(url, windowName) {
     window.open(url, windowName, "popup");
 };
+
+const togglePassword = () => {
+    const passwordInput = document.querySelector("#inputPassword")
+    passwordInput.type = passwordInput.type === "text" ? "password" : "text"
+    const eyeIcon = document.querySelector("#eye")
+    eyeIcon.classList.contains("d-none") ? eyeIcon.classList.remove("d-none") : eyeIcon.classList.add("d-none")
+    const eyeSlashIcon = document.querySelector("#eye-slash")
+    eyeSlashIcon.classList.contains("d-none") ? eyeSlashIcon.classList.remove("d-none") : eyeSlashIcon.classList.add("d-none")
+}
